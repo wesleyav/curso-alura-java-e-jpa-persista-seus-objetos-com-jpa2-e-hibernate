fa0ee0441d21a4340cc7c9b4e0c67fde3920c5ec
Wesley <wesley.contato@hotmail.com>
2020-11-04 07:14:18 -0300

    Estados: Managed e Datached

--- projeto-jpa/src/main/java/br/com/alura/jpa/testes/CriaConta.java
+++ projeto-jpa/src/main/java/br/com/alura/jpa/testes/CriaConta.java
@@ -13,12 +13,20 @@ public class CriaConta {
 		EntityManager em = emf.createEntityManager();
 		
 		Conta conta = new Conta();
-		conta.setTitular("Leonardo");
+		conta.setTitular("Marcia");
 		conta.setNumero(1234);
 		conta.setAgencia(4321);
 		
 		em.getTransaction().begin();
 		em.persist(conta);
 		em.getTransaction().commit();
+		em.close();
+		
+		EntityManager em2 = emf.createEntityManager();
+		System.out.println("ID da conta da Marcia" + conta.getId());
+		
+		em2.getTransaction().begin();
+		em2.merge(conta);
+		em2.getTransaction().commit();
 	}
 }
