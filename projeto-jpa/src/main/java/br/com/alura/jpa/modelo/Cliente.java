b3dd818a465224777cedd39e5a03ae74ffd87779
Wesley <wesley.contato@hotmail.com>
2020-11-04 08:05:20 -0300

    Relacionamento Um para Um

--- projeto-jpa/src/main/java/br/com/alura/jpa/modelo/Cliente.java
+++ projeto-jpa/src/main/java/br/com/alura/jpa/modelo/Cliente.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Cliente {
@@ -14,29 +15,48 @@ public class Cliente {
 	private String nome;
 	private String profissao;
 	private String endereco;
+	
+	@OneToOne
+	private Conta conta;
+
+	public Conta getConta() {
+		return conta;
+	}
+
+	public void setConta(Conta conta) {
+		this.conta = conta;
+	}
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getProfissao() {
 		return profissao;
 	}
+
 	public void setProfissao(String profissao) {
 		this.profissao = profissao;
 	}
+
 	public String getEndereco() {
 		return endereco;
 	}
+
 	public void setEndereco(String endereco) {
 		this.endereco = endereco;
 	}
-	
+
 }
