3a4c9c74f777d6cb7a1f520861320c55c21e3d0c
Wesley <wesley.contato@hotmail.com>
2020-11-04 08:02:52 -0300

    Associando categorias às movimentações

--- projeto-jpa/src/main/java/br/com/alura/jpa/modelo/Movimentacao.java
+++ projeto-jpa/src/main/java/br/com/alura/jpa/modelo/Movimentacao.java
@@ -2,6 +2,7 @@ package br.com.alura.jpa.modelo;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -9,6 +10,7 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 
 @Entity
@@ -24,6 +26,9 @@ public class Movimentacao {
 	private TipoMovimentacao tipoMovimentacao;
 	private LocalDateTime data;
 	private String descricao;
+	
+	@ManyToMany
+	private List<Categoria> categoria;
 
 	@ManyToOne
 	private Conta conta;
