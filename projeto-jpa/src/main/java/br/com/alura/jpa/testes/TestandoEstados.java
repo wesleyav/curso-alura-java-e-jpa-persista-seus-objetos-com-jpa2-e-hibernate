9ef370e3f1321fa609cf1a0dae3a068d99b65c5b
Wesley <wesley.contato@hotmail.com>
2020-11-04 07:23:06 -0300

    O estado Removed

---
+++ projeto-jpa/src/main/java/br/com/alura/jpa/testes/TestandoEstados.java
@@ -0,0 +1,33 @@
+package br.com.alura.jpa.testes;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.com.alura.jpa.modelo.Conta;
+
+public class TestandoEstados {
+
+	public static void main(String[] args) {
+
+		// Transient
+		Conta conta = new Conta();
+		conta.setTitular("Almira");
+		conta.setAgencia(123123);
+		conta.setNumero(12387128);
+		
+		EntityManagerFactory emf =  Persistence.createEntityManagerFactory("contas");
+		EntityManager em = emf.createEntityManager();
+
+		em.getTransaction().begin();
+		
+		// Transient -> Managed
+		em.persist(conta);
+		
+		// Managed -> Removed
+		em.remove(conta);
+		
+		em.getTransaction().commit();
+	}
+
+}
