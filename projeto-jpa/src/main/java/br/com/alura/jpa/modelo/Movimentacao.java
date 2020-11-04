e52fce7dbf8693270df3d2f43536c927a76ca39a
Wesley <wesley.contato@hotmail.com>
2020-11-04 07:50:56 -0300

    Relacionamento Muitos para Um

--- projeto-jpa/src/main/java/br/com/alura/jpa/modelo/Movimentacao.java
+++ projeto-jpa/src/main/java/br/com/alura/jpa/modelo/Movimentacao.java
@@ -9,6 +9,7 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Movimentacao {
@@ -23,6 +24,17 @@ public class Movimentacao {
 	private TipoMovimentacao tipoMovimentacao;
 	private LocalDateTime data;
 	private String descricao;
+
+	@ManyToOne
+	private Conta conta;
+
+	public Conta getConta() {
+		return conta;
+	}
+
+	public void setConta(Conta conta) {
+		this.conta = conta;
+	}
 
 	public Long getId() {
 		return id;
